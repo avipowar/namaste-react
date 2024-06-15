a3fd3d8bebb99b2365ccea7702eaf70ab638db31
powaravinash <powaravinash06@gmail.com>
2024-06-15 12:58:34 +0530

    episode 03

--- app.js
+++ app.js
@@ -14,23 +14,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I AM AVINASH   "),
+// Recat Element
+const num = 10000;
 
-    React.createElement("h1", {}, "I'm h2 ashuss"),
-  ]),
+// React Component Compostions
+const HeadingComponent = () => (
+  <div id="container">
+    {/* {heading} */}
+    <h1 className="heading">Namsate React Functional Component</h1>
+  </div>
+);
 
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-
-    React.createElement("h1", {}, "I'm h2 tag"),
-  ]),
-]);
+const heading =
+  ((<HeadingComponent />),
+  (
+    <h1 className="head" tabIndex="5">
+      <h1>{num}</h1>
+      Hiii I am Avinash
+    </h1>
+  ));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
+
+// const heading = React.createElement("h1", { id: "heaing" }, "This is Avinash");
+// // console.log("heading");
+
+// // use jsx to create react element
+// const jsxHeading = <h1 className="head"> Hiii I am Avinash </h1>;
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I AM AVINASH   "),
+
+//     React.createElement("h1", {}, "I'm h2 vaibhav"),
+//   ]),
+
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm h1 tag"),
+
+//     React.createElement("h1", {}, "I'm h2 tag"),
+//   ]),
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
 
 // const heading = React.createElement(
 //     "h1",
