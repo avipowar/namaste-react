83514757500b68b969eb14e44eff3d05ea5af86e
Avinash Powar <powaravinash06@gmail.com>
2024-07-28 13:02:55 +0530

    add one more test case in custom hooks

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -3,16 +3,14 @@ const useOnlineStatus = () => {
     const [onlineStatus, setOnlineStatus] =  useState(true)
 
     useEffect(() => {
-
         window.addEventListener("offline", () => {
             setOnlineStatus(false);
         })
         window.addEventListener("online", () => {
             setOnlineStatus(true);
         })
-
     }, [])
-    
+
     return onlineStatus
 }
 export default useOnlineStatus
